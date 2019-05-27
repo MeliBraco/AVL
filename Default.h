3748c90e2f247ee11e10ca028600970cc1985a18
MelinaB <32472302+MeliBraco@users.noreply.github.com>
2019-05-27 15:14:53 -0300

    Inicio TP Nº2

---
+++ Default.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct arbol{
+
+    int dato;
+    struct arbol *izquierdo;
+    struct arbol *derecho;
+};
+
+
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "Arbol.c"
+
+int main(){
+
+    struct arbol *un_arbol = NULL;
+    
+    return 0;
+}
