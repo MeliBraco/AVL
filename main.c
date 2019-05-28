0a5c82e212f70417cb1df2845faa2766b4800458
Ivan Mengual <ivanmengual97@gmail.com>
2019-05-28 08:54:36 -0300

    Agregado menu, actualmente inactivo

--- main.c
+++ main.c
@@ -13,3 +13,57 @@ int main(){
 
     return 0;
 }
+void menu(struct arbol *un_arbol){
+    //menu principal
+   int opciones;
+
+   do{
+      printf("\nMENU"
+              "\n1- Agregar nodo"
+              "\n2- Eliminar nodo "
+              "\n3- Mostrar arbol"
+              "\n4- Recorrer el arbol"
+              "\n5- Buscar dato"
+              "\n6- Salir \n "
+              "\nIngrese una Opcion:");
+
+      scanf("%d", &opciones);
+
+      switch(opciones) {
+
+         case 1:
+              int dato;
+              printf("\nIngrese el valor del dato a agregar: ");
+              scanf("%d";&dato);
+            //insertar( *un_arbol, dato);
+         break;
+
+         case 2:
+                //Elimina un dato
+         break;
+
+         case 3:
+            mostrar(un_arbol);
+         break;
+
+         case 4:
+                //debe tener para seleccionar la opcion de pre pos o in order y recorrerlo
+         break;
+
+         case 5:
+                //Estaba en la consigna, no entiendo que tiene que devolver,
+              //supongo que mas datos o meta (la pos en el arbol, el nivel de profundidad, si es hoja (ultimo nodo))
+         break;
+
+         case 6:
+             Salir();
+         break;
+
+         default:
+            printf("Opcion Incorrecta. \n por favor ingrese un numero del 1 al 6 \n");
+      }
+
+   }while(opciones !=6);
+}
+
+}
