bbb8e6d75ed3bfccfacf8f7e603046d437ff1407
Ivan Mengual <ivanmengual97@gmail.com>
2019-05-29 20:17:36 -0300

    METODO ELIMINAR ESTA MAL

--- Arbol.c
+++ Arbol.c
@@ -57,12 +57,28 @@ int buscar(struct arbol *MiArbol, int dato){
     }
 }
 
+int buscarMinimo(struct arbol *miArbol){
+    if (miArbol->izquierdo != NULL){
+        buscarMinimo(miArbol->izquierdo);
+    }else{
+        return miArbol->dato;
+    }
 
-int eliminar(struct arbol *miArbol, int datoAeliminar) {
+}
+
+struct arbol* eliminar(struct arbol *miArbol, int datoAeliminar) {
     //metodo para eliminar un nodo del arbol, devuelve 0 si no encontro el dato (para lanzar excepcion)
     //me fijo si el arbol esta vacio
     if (miArbol == NULL){
-        return 0;
+        return miArbol;
+        printf("El arbol esta vacio, no hay nada que eliminar");
+    }
+    //busco recursivamente el dato
+    if (miArbol->dato < datoAeliminar){
+        miArbol->izquierdo = eliminar(miArbol->izquierdo, datoAeliminar);
+    }
+    if (miArbol->dato > datoAeliminar) {
+        miArbol->derecho = eliminar(miArbol->derecho, datoAeliminar);
     }
     if (miArbol->dato == datoAeliminar){
         //si no tiene hijos
@@ -73,17 +89,22 @@ int eliminar(struct arbol *miArbol, int datoAeliminar) {
              y los punteros que apuntaban al dato ahora van a NULL
              */
             free(miArbol);
-        }// falta completar para cuando tiene hijos
+        }// Cuando tiene un solo hijo
+        if (miArbol->izquierdo == NULL && miArbol->derecho != NULL) {
+            miArbol = miArbol->derecho;
+        }
+        if (miArbol->izquierdo != NULL && miArbol->derecho == NULL) {
+            miArbol = miArbol->izquierdo;
+        }//Cuando tiene los dos hijos
+        if (miArbol->izquierdo != NULL && miArbol->derecho != NULL) {
+            int temp = buscarMinimo(miArbol->derecho);
+            miArbol->dato = temp;
+            eliminar(miArbol->derecho, temp);
+        }
 
 
     }
-    //busco recursivamente el dato
-    if (miArbol->dato < datoAeliminar){
-        miArbol->izquierdo = eliminar(miArbol->izquierdo, datoAeliminar);
-    }
-    if (miArbol->dato > datoAeliminar) {
-        miArbol->derecho = eliminar(miArbol->derecho, datoAeliminar);
-    }
+
 
 }
 
